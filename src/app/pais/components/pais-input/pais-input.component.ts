0fb10845428b1860fb64abc7d32771d28a466154
unknown <fazahuanchef@gmail.com>
2022-05-19 15:04:08 -0500

    creando componente input y tabla

---
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pais-input',
+  templateUrl: './pais-input.component.html',
+  styles: [
+  ]
+})
+export class PaisInputComponent implements OnInit {
+  termino: string = '';
+  buscar() {
+
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
